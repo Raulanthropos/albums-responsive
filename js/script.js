12d4a44b4757e5e977b14c3910229667647bfeb7
Raulopoulos <62166476+Raulopoulos@users.noreply.github.com>
2022-11-04 22:20:16 +0200

    fixed the dlt-btn functionality

--- js/script.js
+++ js/script.js
@@ -1,100 +1,12 @@
-const deleteButton1 = document.getElementById("dlt-btn-1");
-const deleteButton2 = document.getElementById("dlt-btn-2");
-const deleteButton3 = document.getElementById("dlt-btn-3");
-const deleteButton4 = document.getElementById("dlt-btn-4");
-const deleteButton6 = document.getElementById("dlt-btn-6");
-const deleteButton7 = document.getElementById("dlt-btn-7");
-const deleteButton8 = document.getElementById("dlt-btn-8");
-const deleteButton9 = document.getElementById("dlt-btn-9");
-const deleteButton10 = document.getElementById("dlt-btn-10");
-const deleteButton11 = document.getElementById("dlt-btn-11");
-const deleteButton12 = document.getElementById("dlt-btn-12");
-const deleteButton13 = document.getElementById("dlt-btn-13");
-const deleteButton14 = document.getElementById("dlt-btn-14");
-const deleteButton15 = document.getElementById("dlt-btn-15");
-const deleteButton16 = document.getElementById("dlt-btn-16");
-const deleteButton17 = document.getElementById("dlt-btn-17");
-const ulRow1 = document.getElementById("ul-row-1");
-const ulRow2= document.getElementById("ul-row-2");
-const ulRow3 = document.getElementById("ul-row-3");
-const ulRow4 = document.getElementById("ul-row-4");
-const ulRow6 = document.getElementById("ul-row-6");
-const ulRow7 = document.getElementById("ul-row-7");
-const ulRow8 = document.getElementById("ul-row-8");
-const ulRow9 = document.getElementById("ul-row-9");
-const ulRow10 = document.getElementById("ul-row-10");
-const ulRow11 = document.getElementById("ul-row-11");
-const ulRow12 = document.getElementById("ul-row-12");
-const ulRow13 = document.getElementById("ul-row-13");
-const ulRow14 = document.getElementById("ul-row-14");
-const ulRow15 = document.getElementById("ul-row-15");
-const ulRow16 = document.getElementById("ul-row-16");
-const ulRow17 = document.getElementById("ul-row-17");
-
-const removeRow1 = () => {
-    ulRow1.innerHTML = "";
+const parentList = document.querySelectorAll(".listofelements"); //All unordered lists hosting all list items with the delete button as the last item
+const removeRowButtons = document.querySelectorAll(".row-btn-tbd"); //All delete buttons
+const removeRow = () => {
+for (let i=0; i<removeRowButtons.length; i++) { //Iterating through the buttons
+    removeRowButtons[i].addEventListener('click', () => { //Event listener within the iteration
+        parentList[i].remove(); //Same number of unordered lists and removebuttons - by clicking the button, on line 103, we ask the listener to delete the whole associated row
+    })
 }
-const removeRow2 = () => {
-    ulRow2.innerHTML = "";
-}
-const removeRow3 = () => {
-    ulRow3.innerHTML = "";
-}
-const removeRow4 = () => {
-    ulRow4.innerHTML = "";
-}
-const removeRow6 = () => {
-    ulRow6.innerHTML = "";
-}
-const removeRow7 = () => {
-    ulRow7.innerHTML = "";
-}
-const removeRow8 = () => {
-    ulRow8.innerHTML = "";
-}
-const removeRow9 = () => {
-    ulRow9.innerHTML = "";
-}
-const removeRow10 = () => {
-    ulRow10.innerHTML = "";
-}
-const removeRow11 = () => {
-    ulRow11.innerHTML = "";
-}
-const removeRow12 = () => {
-    ulRow12.innerHTML = "";
-}
-const removeRow13 = () => {
-    ulRow13.innerHTML = "";
-}
-const removeRow14 = () => {
-    ulRow14.innerHTML = "";
-}
-const removeRow15 = () => {
-    ulRow15.innerHTML = "";
-}
-const removeRow16 = () => {
-    ulRow16.innerHTML = "";
-}
-const removeRow17 = () => {
-    ulRow17.innerHTML = "";
 }
 
-deleteButton1.addEventListener('click', removeRow1);
-deleteButton2.addEventListener('click', removeRow2);
-deleteButton3.addEventListener('click', removeRow3);
-deleteButton4.addEventListener('click', removeRow4);
-deleteButton6.addEventListener('click', removeRow6);
-deleteButton7.addEventListener('click', removeRow7);
-deleteButton8.addEventListener('click', removeRow8);
-deleteButton9.addEventListener('click', removeRow9);
-deleteButton10.addEventListener('click', removeRow10);
-deleteButton11.addEventListener('click', removeRow11);
-deleteButton12.addEventListener('click', removeRow12);
-deleteButton13.addEventListener('click', removeRow13);
-deleteButton14.addEventListener('click', removeRow14);
-deleteButton15.addEventListener('click', removeRow15);
-deleteButton16.addEventListener('click', removeRow16);
-deleteButton17.addEventListener('click', removeRow17);
-
+removeRow();
 
